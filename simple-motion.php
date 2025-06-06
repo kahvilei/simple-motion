5a86bbabfc5c39277ab1a12e2dbf74d8154e960b
Katie Huff <luovakatie@gmail.com>
2025-06-06 11:08:42 -0400

    first commit

---
+++ simple-motion.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:         Simple Motion
+ * Description:         Adds simple exit and entrance animation options and positioning features for default blocks.
+ * Author:              Katie Britton
+ * Version:             0.1.0
+ * Requires at least:   6.5
+ */
